8822e4111965f5a6916acb5dac862dc9e6ee2110
Rishabh Tater <rishabhtater14@gmail.com>
2021-08-04 12:42:14 +0530

    Updates

---
+++ precache-manifest.ce2bfdcfa8e4cca9afdcefe3a06b3bd5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09dcc2cf630fab9b0281f0ac63961516",
+    "url": "/AlooParantha/index.html"
+  },
+  {
+    "revision": "559c437c4a453b2904c0",
+    "url": "/AlooParantha/static/js/2.e758081c.chunk.js"
+  },
+  {
+    "revision": "716eed7b959950beb503",
+    "url": "/AlooParantha/static/js/main.b8a19d5e.chunk.js"
+  },
+  {
+    "revision": "771de83e725b73d61f7f",
+    "url": "/AlooParantha/static/js/runtime-main.f964413c.js"
+  }
+]);
